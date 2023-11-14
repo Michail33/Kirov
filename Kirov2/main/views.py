4184697cc23b4c2c03372a8575c683cb2dc2109f
Michail <mike.fireman33@gmail.com>
2023-11-14 19:22:26 +0300

    20231114 Первая загрузка

---
+++ Kirov2/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+def index(request):
+    # return HttpResponse('<h1> Главная страница! </h1>')
+    return render(request, 'main/index.html')
+def about(request):
+    # return HttpResponse('<h1> О нас </h1>')
+    return render(request, 'main/about.html')
+def contacts(request):
+    # return HttpResponse('<h1> Контакты </h1>')
+    return render(request, 'main/contacts.html')
+def content(request):
+    # return HttpResponse('<h1> Содержание </h1>')
+    return render(request, 'main/content.html')
