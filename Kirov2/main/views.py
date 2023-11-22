9a49705de5d85f7834c39eed4878708ee130ed93
Michail <mike.fireman33@gmail.com>
2023-11-22 15:01:56 +0300

    20231121 подключен Bootstrap, созданы контейнеры

--- Kirov2/main/views.py
+++ Kirov2/main/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     # return HttpResponse('<h1> Главная страница! </h1>')
-    return render(request, 'main/index.html')
+    return render(request, 'main/sidebar.html')
 def about(request):
     # return HttpResponse('<h1> О нас </h1>')
     return render(request, 'main/about.html')
